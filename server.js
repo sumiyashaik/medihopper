ee988f53d891417181f26810c19a03271dca2d73
Prem G <sir_prem@hotmail.com>
2021-08-21 00:13:43 +1000

    basic boilerplate code with header and footer, express server

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
-const express = require("express");
-const app = express();
-const http = require('http');
-const server = http.createServer(app);
+var express = require("express");
+var mongoose = require("mongoose");
+var path = require("path");
+//var bodyParser = require("body-parser");
+var cookieParser = require("cookie-parser");
+var session = require("express-session");
+var flash = require("connect-flash");
 
-const port = 4000;
-
+var routes = require("./routes");
+var app = express();
 
 var uri = "mongodb+srv://cluster0.7hvms.mongodb.net/";
-
-const mongoose = require("mongoose");
 
 mongoose
   .connect(
@@ -28,16 +29,23 @@ mongoose
       }
   )
 
-let homePage = require('./Views/homepage');
+const port = 4000;
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({ limit: "200mb",  extended: true, parameterLimit: 1000000 }));
-app.use(express.static(__dirname + '/Views'));
+app.use(cookieParser());
+app.use(session ({
+  secret: "asdjDsLKsjkjJlkK3*32h#$%wlkj@#s.<<MX",
+  resave: true,
+  saveUninitialized: true
+}));
+app.use(flash());
+app.use(routes);
+//app.use(express.static(__dirname + '/Views'));
+//app.use(bodyParser.urlencoded({extended: false }));
 
-app.get("/", async (req, res, next) => {
-  await homePage.displayHomePage(res);
-});
-
-server.listen(port, function() {
-  console.log("Server is running on Port: " + port);
+app.listen(port, function() {
+  console.log("Server is running on port: " + port);
 });
